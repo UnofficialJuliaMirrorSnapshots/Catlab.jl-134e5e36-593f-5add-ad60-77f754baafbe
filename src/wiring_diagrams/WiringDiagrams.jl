952cccc4bbb81fa9c4a1339634bccd4e15a067d8
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-07 19:43:01 +0000

    Snapshot of branch master taken on 2019-04-07T15:43:00.388-04:00 from "https://github.com/UnofficialJuliaMirror/Catlab.jl-134e5e36-593f-5add-ad60-77f754baafbe"

---
+++ src/wiring_diagrams/WiringDiagrams.jl
@@ -0,0 +1,22 @@
+module WiringDiagrams
+
+using Reexport
+
+include("Core.jl")
+include("Layers.jl")
+include("Algorithms.jl")
+include("Serialization.jl")
+include("GraphML.jl")
+include("JSON.jl")
+
+@reexport using .WiringDiagramCore
+@reexport using .WiringLayers
+@reexport using .WiringDiagramAlgorithms
+
+using .WiringDiagramSerialization
+export convert_from_graph_data, convert_to_graph_data
+
+@reexport using .GraphMLWiringDiagrams
+@reexport using .JSONWiringDiagrams
+
+end
---
+++ test/wiring_diagrams/WiringDiagrams.jl
@@ -0,0 +1,21 @@
+using Test
+
+@testset "Core" begin
+  include("Core.jl")
+end
+
+@testset "Layers" begin
+  include("Layers.jl")
+end
+
+@testset "Algorithms" begin
+  include("Algorithms.jl")
+end
+
+@testset "GraphML" begin
+  include("GraphML.jl")
+end
+
+@testset "JSON" begin
+  include("JSON.jl")
+end
